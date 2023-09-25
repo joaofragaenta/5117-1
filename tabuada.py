7c09f219af2d37d19e237b3f4e4524843df984c6
João Fraga <joaovitorfraga@enta.pt>
2023-09-25 11:59:34 +0000

    Removed redundant lines from program

--- tabuada.py
+++ tabuada.py
@@ -1,8 +1,4 @@
-
 for elemento2 in range(10):
     print(f"----- Tabuada dos {elemento2} -----")
-    elemento1 = 1
     for elemento1 in range(10):
         print(f"{elemento2} x {elemento1} = {elemento1*elemento2}")
-        elemento1 += 1
-    elemento2 += 1
