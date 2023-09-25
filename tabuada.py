67a9400c7b707e0cddb323fa50d9705b734812f0
João Fraga <joaovitorfraga@enta.pt>
2023-09-25 11:58:16 +0000

    Further simplified tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,10 +1,8 @@
 
-elemento1 = 1
-elemento2 = 1
-while elemento2 < 10:
+for elemento2 in range(10):
     print(f"----- Tabuada dos {elemento2} -----")
     elemento1 = 1
-    while elemento1 < 10:
+    for elemento1 in range(10):
         print(f"{elemento2} x {elemento1} = {elemento1*elemento2}")
         elemento1 += 1
     elemento2 += 1
