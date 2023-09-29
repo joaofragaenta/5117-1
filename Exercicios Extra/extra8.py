58a1de19d1e95b58cbf826586efc5c763583852d
João Fraga <joaovitorfraga@enta.pt>
2023-09-29 08:37:01 +0000

    Removed try catch blocks

--- Exercicios Extra/extra8.py
+++ Exercicios Extra/extra8.py
@@ -1,10 +1,6 @@
 input_number = 0
 while input_number >= 0:
     input_number = int(input("Escreva um número de segundos: "))
-    try:
-        input_number -1
-    except Exception as e:
-        print(e)
     
     print(f"O número de dias correspondente é {input_number /60 /60 / 24}")
 
--- Exercicios Extra/extra9.py
+++ Exercicios Extra/extra9.py
@@ -2,10 +2,6 @@ final_number = 0
 input_number = 0
 while input_number >= 0:
     input_number = int(input("Escreava um dígito\n(-1 para terminar)"))
-    try:
-        input_number -1
-    except Exception as e:
-        print(e)
 
     if input_number >= 0:
         final_number = final_number * 10 + input_number
