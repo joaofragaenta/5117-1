e47f0fbce0499010427c38ee8c8e5de8820a5fa7
João Fraga <joaovitorfraga@enta.pt>
2023-10-02 10:32:04 +0000

    Added "range()" and string case functions

---
+++ Exercicios da aula/Funcoes Nativas/range.py
@@ -0,0 +1,4 @@
+t = [1,2,3,4,5,6,7,8,9]
+
+for i in range(0, len(t), 2):
+    print(f"t[{i}] = {t[i]}")
---
+++ Exercicios da aula/Funcoes Nativas/string_case_functions.py
@@ -0,0 +1,7 @@
+nome = "escola de novas tecnologias dos Açores"
+print(nome.capitalize())
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+print(nome.swapcase())
+print(nome.replace("Açores", "Açores, Portugal"))
