dde6317fff60bc34fb0e638c9245f782415d1b1b
João Fraga <joaovitorfraga@enta.pt>
2023-10-02 11:43:17 +0000

    Added 'Dict' folder along with the text_stats exercise

---
+++ Exercicios da aula/Dicts/text_stats.py
@@ -0,0 +1,33 @@
+texto = input("Escreava um pequeno texto para calculo de estatisticas: \n")
+
+letter_occurence = {}
+for letra in texto:
+    if letra in letter_occurence:
+        letter_occurence[str(letra).lower()] += 1
+    elif not letra == ' ':
+        letter_occurence[str(letra).lower()] = 1
+
+
+
+
+#Quantas vezes aparece cada letra?
+print("Ocorrência de letras:\n")
+for i in letter_occurence:
+    print(f"{i}: {letter_occurence[i]}")
+
+#Quantas vezes aparece cada letra do maior para menor?
+
+sorted_dict = {}
+keys_to_ignore = []
+
+for i in letter_occurence:
+    highest_key = ""
+    highest_value = 0
+    for x in letter_occurence.keys():
+        if letter_occurence[x] > highest_value and not (x in keys_to_ignore):
+            highest_value = letter_occurence[x]
+            highest_key = x
+    sorted_dict[highest_key] = highest_value
+    keys_to_ignore.append(highest_key)
+
+print(sorted_dict)
