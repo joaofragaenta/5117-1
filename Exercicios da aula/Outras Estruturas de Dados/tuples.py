01733954022b72dacf87069d332e1113583ea164
João Fraga <joaovitorfraga@enta.pt>
2023-10-02 10:31:39 +0000

    Added tuples exercise

---
+++ Exercicios da aula/Outras Estruturas de Dados/tuples.py
@@ -0,0 +1,17 @@
+t = (12, 10, 'Maria', 20, 0o101, 0x1A)
+
+#for x in t:
+#   print(x)
+
+
+t2 = ([[12 , 33 , 44 , 5],23], 18, 'Maria', 0o20, 0xA)
+
+print(t2[0][0][0])
+print(t2[0][0][1])
+print(t2[0][0][2])
+print(t2[0][0][3])
+print(t2[0][1])
+print(t2[1])
+print(t2[2])
+print(t2[3])
+print(t2[4])
