8569fb312ed6dfdb8edbb00843e7cdf6604ed65d
João Fraga <joaovitorfraga@enta.pt>
2023-10-03 12:10:43 +0000

    Better ex20

--- Exercicios Extra/extra20.py
+++ Exercicios Extra/extra20.py
@@ -1,5 +1,6 @@
 first_value = ""
+tabuada = 8
 
 for i in range(1,10):
     first_value = first_value + str(i)
-    print(f"{first_value} x 8 + {i} = {int(first_value)*8+i}")
+    print(f"{first_value} x {tabuada} + {i} = {int(first_value)*tabuada+i}")
